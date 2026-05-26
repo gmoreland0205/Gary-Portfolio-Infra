1399ba976ab4f903a07d30c19f67d14cafa04ffa
gmoreland0205 <gary_mm@hotmail.com>
2026-05-25 21:41:41 -0400

    Added bucket policy to Allow Cloud Access

--- module/download_resume/main.tf
+++ module/download_resume/main.tf
@@ -91,3 +91,31 @@ resource "aws_cloudfront_distribution" "cloudfront" {
   }
   
 }
+
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
+  bucket = aws_s3_bucket.resume_files_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+
+        Action = "s3:GetObject"
+
+        Resource = "${aws_s3_bucket.resume_files_bucket.arn}/*"
+
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cloudfront.arn
+          }
+        }
+      }
+    ]
+  })
+}
